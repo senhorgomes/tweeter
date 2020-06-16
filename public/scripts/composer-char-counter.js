09dff3ff3c311909821ff7f9570db5e2cdda8ff6
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-16 21:43:14 +0000

    Started working on how to reduce counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   // --- our code goes here ---
   const textArea = $('#tweet-text');
+  const counter = $('.counter');
   textArea.on('keydown', function() {
-    console.log(this.value.length); //The this keyword is a reference to the button
+    console.log(counter.value - this.value.length); //The this keyword is a reference to the button
   });
 });
