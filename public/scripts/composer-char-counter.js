003bbe74b7ea518c20343ec0d8fb5554f3c82eb5
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-16 23:13:27 +0000

    Added comments to explain functionality better

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   // --- our code goes here ---
   const textArea = $('#tweet-text');
   const counter = $('.counter');
+  //Using keyUp as key up is before the computer runs the calculation
   textArea.on('keyup', function() {
-    // console.log(140 - (this.value.length));
+    //There is a more direct approach, $('.counter') directly. But its bad practice
+    //Will go up to its siblings, and grab the value of the child counter. it will then change it according to the length of characters in the text box.
     textArea.siblings('div').children('.counter').val(140 - (this.value.length))
-    //The this keyword is a reference to the button
+
   });
 
 });
