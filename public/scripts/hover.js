f97daf4cb236d7376e892852daefdcfa6b60da1d
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-17 16:34:26 +0000

    Figured out how to implement the hover functionality, will change color later

--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
-  const tweetArea = $('.old-tweet');
-  //Using keyUp as key up is before the computer runs the calculation
-  tweetArea.on('mouseover', function() {
-    //There is a more direct approach, $('.counter') directly. But its bad practice
-    //Will go up to its siblings, and grab the value of the child counter. it will then change it according to the length of characters in the text box.
-    tweetArea.css('box-shadow', '5px 10px');
-  });
+// $(document).ready(function() {
+//   const tweetArea = $('.old-tweet');
+//   //Using keyUp as key up is before the computer runs the calculation
+//   tweetArea.on('mouseover', function() {
+//     //There is a more direct approach, $('.counter') directly. But its bad practice
+//     //Will go up to its siblings, and grab the value of the child counter. it will then change it according to the length of characters in the text box.
+//     tweetArea.css('box-shadow', '5px 10px');
+//   });
 
-});
+// });
