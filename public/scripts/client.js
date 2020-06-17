2319f8a0cf088a07591643a2ef660e77073c1829
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-17 23:07:23 +0000

    Ajax is now implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -62,17 +62,6 @@ $(document).ready(function () {
   renderTweets(data);
   $('form').on('submit', (evt) => {
     evt.preventDefault();
-    // How do I get the value of what i typed INTO my search URL?
-    // How do i bundle the data i need to send to the user  ( hint hint serilize? )
-    $.ajax({
-        url: `http://api.tvmaze.com/search/shows?q=${evt.target.search.value}`,
-        method: 'GET',
-        dataType: 'JSON'
-    }).then(function(response) {
-        console.log(response);
-        // const item = createItem(response[0])
-        $('#results').empty();
-        createItems(response);
-    })
+    $.ajax("/tweets", { method: 'POST' , data: $(this).serialize()})
   })
 });
