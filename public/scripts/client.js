077ae4baf73d3fe9140e7e181a2cd7dfacfc5e75
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-17 19:46:26 +0000

    createArticle function is done, proceeding with createElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,28 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createArticle = (data) => {
+  const article = `<article class="tweet-container">
+  <header class="old-tweet-userinfo">
+    <div>
+      <img src=${data.user.avatars}>
+      <p>${data.user.name}</p>
+    </div>
+    <p class="Username">${data.user.handle}</p>
+  </header>
+  <p class="tweet">
+    ${data.content.text}
+  </p>
+  <footer>
+  <p class="dateSinceTweet">
+    ${data.created_at}
+  </p>
+  </footer>
+</article>`;
+
+return article
+}
+
+const createTweetElement = (data) => {
+}
 const $tweet = $(`<article class="tweet">Hello world</article>`);
