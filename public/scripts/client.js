46224684386ed7781c6024e36f28850efcc439bb
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-18 01:44:37 +0000

    loadTweets get function is up, however new tweets wont post now

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,37 +33,12 @@ const renderTweets = (tweets) => {
   }
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const loadTweets = () => {
-  $.ajax('/tweets', { method: 'POST', dataType: 'JSON' , success: renderTweets })
+  $.ajax('/tweets', { method: 'GET', dataType: 'JSON', success: renderTweets })
 };
 
 $(document).ready(function () {
-  renderTweets(data);
+  loadTweets();
   $('form').on('submit', (evt) => {
     evt.preventDefault();
     //Action and method have to match the form that we are grabbing the information from
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -18,6 +18,8 @@ module.exports = function(DataHelpers) {
   });
 
   tweetsRoutes.post("/", function(req, res) {
+    console.log("Posted,", req.body.text);
+    
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
