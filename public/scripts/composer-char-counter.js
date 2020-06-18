13aaa8549f1457e7fae150ee3df2be8e4572fe16
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-18 18:40:39 +0000

    Fixed counter bug. Counter would turn red after 140 characters limit was reach, and wouldnt revert back if characters were deleted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,8 +6,12 @@ $(document).ready(function() {
     //There is a more direct approach, $('.counter') directly. But its bad practice
     //Will go up to its siblings, and grab the value of the child counter. it will then change it according to the length of characters in the text box.
     textArea.siblings('div').children('.counter').val(140 - (this.value.length))
+    //Conditional to change to change text color if the character count goes above 140
+    //Had an issue where it would go red and not switch back, added else to keep it the original color
     if ((this.value.length) > 140) {
       textArea.siblings('div').children('.counter').css('color', '#ff0000');
+    } else {
+      textArea.siblings('div').children('.counter').css('color', '#545149');
     }
   });
 
