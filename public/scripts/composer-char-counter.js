303ab52655b848ec5b0273670f1f05198c93a958
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-19 05:11:48 +0000

    Linted code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   textArea.on('keyup', function() {
     //There is a more direct approach, $('.counter') directly. But its bad practice
     //Will go up to its siblings, and grab the value of the child counter. it will then change it according to the length of characters in the text box.
-    textArea.siblings('div').children('.counter').val(140 - (this.value.length))
+    textArea.siblings('div').children('.counter').val(140 - (this.value.length));
     //Conditional to change to change text color if the character count goes above 140
     //Had an issue where it would go red and not switch back, added else to keep it the original color
     if ((this.value.length) > 140) {
@@ -14,5 +14,4 @@ $(document).ready(function() {
       textArea.siblings('div').children('.counter').css('color', '#545149');
     }
   });
-
 });
