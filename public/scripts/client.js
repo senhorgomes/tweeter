1e36f23434a89b4e7ebc798d7a502e23576bd9fd
Bryan Gomes <bryanpgomes@gmail.com>
2020-06-28 17:16:34 +0000

    Removed extra comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,39 +31,28 @@ const createTweetElement = (tweetData) => {
 };
 
 //Render tweet function uses a helper function to produce the tweet in the proper format, and push them into the html. The classes are predone so CSS can do its job properly"
-
 const renderTweets = (tweets) => {
   for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-
-    //Had an issue where the css style was applied to the article, but it actually needs to be applied to the old tweet section class
-    
+    const $tweet = createTweetElement(tweet);    
     $('.old-tweet').prepend($tweet);
   }
 };
 
-const loadTweets = () => {
-
-  //This will retrieve the tweets in the database, and render them with a helper function
-  
+//This will retrieve the tweets in the database, and render them with a helper function
+const loadTweets = () => {  
   $.ajax('/tweets', { method: 'GET', dataType: 'JSON', success: renderTweets });
 };
 
 //To keep things DRY- timedSlideUp helps to hide the alert message after a certain time has reached
 //Delay is just long enough so that the user can read it, and allows another alert to pop up if necessary
-
 const timedSlideUp = () => {
   $('.alert-message').delay(3500).slideUp('fast');
 };
 
 //Prepares page, and renders all tweets on page
-
 $(document).ready(function() {
   $('.alert-message').hide().removeClass("hidden");
   $('form').on('submit', (evt) => {
-
-    //Prevents page refresh
-    
     evt.preventDefault();
     let tweetText = $('#tweet-text').val();
 
@@ -80,8 +69,8 @@ $(document).ready(function() {
     } else {
 
       //Action and method have to match the form that we are grabbing the information from
-      //Serialize will turn the form data into a query string
       //Empties tweets, and resets counter, before loading them again
+
       $.ajax("/tweets", { method: 'POST', data: $('form')
       .serialize() })
       .then(() => {$('.old-tweet')
